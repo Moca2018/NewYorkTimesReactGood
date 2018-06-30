9da7a21adb8b9ad76dc77381b1f1996dbb177706
Nelson Quinones <nelson.quinones@yahoo.com>
2018-06-30 10:02:00 -0400

    not finished changes

---
+++ server.js
@@ -0,0 +1,102 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+var axios = require("axios");
+
+var PORT = 3003;
+
+
+// Require all models
+var db = require("./models");
+
+// Initialize Express
+var app = express();
+
+// Use morgan logger for logging requests
+app.use(logger("dev"));
+
+// Use body-parser for handling form submissions
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+// Use express.static to serve the public folder as a static directory
+app.use(express.static("public"));
+
+// Connect to the Mongo DB
+mongoose.connect("mongodb://localhost/newYorkTimesReactDb");
+
+//====================CREATE ARTICLE=============================
+// When the server starts, create and save a new User document to the db
+// The "unique" rule in the User model's schema will prevent duplicate users from being added to the server
+db.Article.create({ title: "fakeTitle", url: "String" })
+  .then(function(dbArticle) {
+    console.log(dbArticle);
+  })
+  .catch(function(err) {
+    console.log(err.message);
+  });
+
+
+
+
+//=====================ROUTES==========================================
+
+// Route for retrieving all Articles from the db
+app.get("/", function(req, res) {
+    // Find all Articles
+    db.Article.find({})
+      .then(function(dbArticle) {
+        // If all Notes are successfully found, send them back to the client
+        res.json(dbArticle);
+      })
+      .catch(function(err) {
+        // If an error occurs, send the error back to the client
+        res.json(err);
+      });
+  });
+
+
+// Scrape data from one site and place it into the mongodb db
+app.get("/scrape", function(req, res) {
+    // Make a request for the news section of `ycombinator`
+    request("https://www.nytimes.com/", function(error, response, html) {
+      // Load the html body from request into cheerio
+      var $ = cheerio.load(html);
+      // For each element with a "title" class
+      $(".title").each(function(i, element) {
+        // Save the text and href of each link enclosed in the current element
+        var title = $(element).children("a").text();
+        var link = $(element).children("a").attr("href");
+  
+        // If this found element had both a title and a link
+        if (title && link) {
+          // Insert the data in the scrapedData db
+          db.scrapedData.insert({
+            title: title,
+            link: link
+          },
+          function(err, inserted) {
+            if (err) {
+              // Log the error if one is encountered during the query
+              console.log(err);
+            }
+            else {
+              // Otherwise, log the inserted data
+              console.log(inserted);
+            }
+          });
+        }
+      });
+     });
+
+  // Send a "Scrape Complete" message to the browser
+  res.send("Scrape Complete");
+});
+
+
+
+// Start the server
+app.listen(PORT, function() {
+    console.log("App running on port " + PORT + "!");
+  });
